cb0579ce3310a73182fbb962df51de5c13d087c4
Alex Samuel <alex@alexsamuel.net>
2015-03-15 18:20:20 -0400

    division and triangles

---
+++ src/nyc/c4q/ac21/ac20150315/Division.java
@@ -0,0 +1,14 @@
+package nyc.c4q.ac21.ac20150315;
+
+/**
+ * Created by samuel on 2015-03-15.
+ */
+public class Division {
+    public static void main(String[] args) {
+        int runningSum = 31;
+        int count = 6;
+
+        double average = runningSum / (double) count;
+        System.out.println(average);
+    }
+}
